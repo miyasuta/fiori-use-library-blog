23b8fa80bad1650a2ed16e530aa70e72e702cce0
Mio <mio@MionoMacBook-Pro.local>
2026-01-11 05:21:23 +0900

    use library

--- consumerapp/webapp/controller/View1.controller.ts
+++ consumerapp/webapp/controller/View1.controller.ts
@@ -1,4 +1,6 @@
 import Controller from "sap/ui/core/mvc/Controller";
+import MyDialogHanlder, { MyDialogHandler$SubmitEvent } from "com/myorg/reuselib/controller/MyDialogHandler";
+import MessageToast from "sap/m/MessageToast";
 
 /**
  * @namespace miyasuta.consumerapp.controller
@@ -7,6 +9,16 @@ export default class View1 extends Controller {
 
     /*eslint-disable @typescript-eslint/no-empty-function*/
     public onInit(): void {
+
+    }
+
+    public onOpenDialog(): void {
+        const dialogHandler = new MyDialogHanlder();
+        dialogHandler.attachSubmit((event: MyDialogHandler$SubmitEvent) => {
+            const message = event.getParameter("message");
+            MessageToast.show(`Dialog submitted with message: ${message}`);
+        });
+        dialogHandler.open();
 
     }
 }
